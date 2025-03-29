2acdebc4fe9970274aa80bc619d3e35c61e9c58c
kevinlim186 <kevinlim186@gmail.com>
2025-03-29 10:41:45 +0100

    added market cap viz

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dash import Dash, html, dcc, Input, Output, callback
-from pages import german_10_year_breakeven_inflation, german_10_year_inflation_protected_rate,german_10_year_bonds
+from pages import german_10_year_breakeven_inflation, german_10_year_inflation_protected_rate,german_10_year_bonds, sp500_total_revenue
 from flask_caching import Cache
 
 
@@ -28,6 +28,10 @@ def german_10_year_inflation_protected_rate_cache():
 def german_10_year_breakeven_inflation_cache():
     return german_10_year_breakeven_inflation.layout()
 
+@cache.memoize()
+def sp500_total_revenue_cache():
+    return sp500_total_revenue.layout()
+
 
 from flask import request
 
@@ -39,6 +43,8 @@ def refresh_cache():
     german_10_year_inflation_protected_rate_cache()
     cache.delete_memoized(german_10_year_breakeven_inflation_cache)
     german_10_year_breakeven_inflation_cache()
+    cache.delete_memoized(sp500_total_revenue_cache)
+    sp500_total_revenue_cache()
     return "Cache has been refreshed", 200
 
 @app.callback(Output('page-content', 'children'), Input('url', 'pathname'))
@@ -49,6 +55,8 @@ def display_page(pathname):
         return german_10_year_inflation_protected_rate_cache()
     elif pathname == '/german-10-year-breakeven-inflation':
         return german_10_year_breakeven_inflation_cache()
+    elif pathname == '/sp500-total-revenue':
+        return sp500_total_revenue_cache()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8050)
