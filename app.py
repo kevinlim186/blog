929829a7a69b606a9fd2a7bdc0267414e6a97007
kevinlim186 <kevinlim186@gmail.com>
2025-10-23 08:06:19 -0500

    explicit specification of imports

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dash import Dash, html, dcc, Input, Output, callback
-from pages import *
+from pages import commitment_of_traders_eur_forcast, german_10_year_breakeven_inflation, german_10_year_inflation_protected_rate,german_10_year_bonds, german_breakeven_eurusd, philippine_instant_3_in_1_coffee_price,telecom_interest_sensitive_stock, wilshire_cumulative_change, wilshire_net_income, us_companies_cashflow_tax, capital_expenditure, interest_rate_differential_eur_usd, free_cash_flow_to_debt, commitment_of_traders, philippine_rice_price, philippine_egg_price, philippine_milk_price, philippine_instant_noodles_price
 from cache import cache
 from flask import request
 import data.queries as dq
